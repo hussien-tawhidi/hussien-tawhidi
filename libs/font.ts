79fa3f7e873fc1a3627505230b3a3090158b12a1
hussien tawhidi <hussientawhidi710@gmail.com>
2025-10-26 02:37:58 +0330

    first comment

--- libs/font.ts
+++ libs/font.ts
@@ -3,23 +3,23 @@ import localFont from "next/font/local";
 export const shabname = localFont({
   src: [
     {
-      path: "../../fonts/shabname/Shabnam.woff",
+      path: "../fonts/shabname/Shabnam.woff",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../fonts/shabname/Shabnam-light.woff",
+      path: "../fonts/shabname/Shabnam-light.woff",
       weight: "500",
       style: "normal",
     },
 
     {
-      path: "../../fonts/shabname/Shabnam-medium.woff",
+      path: "../fonts/shabname/Shabnam-medium.woff",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../fonts/shabname/Shabnam-bold.woff",
+      path: "../fonts/shabname/Shabnam-bold.woff",
       weight: "900",
       style: "normal",
     },
